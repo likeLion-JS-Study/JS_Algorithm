e10331d56d8d6bab1f4575ffb7697edd8fa7752e
Hwang Juhee <julia0276@naver.com>
2023-01-06 00:59:14 +0900

    [🍒 cherrypick] 9498 bohwa 파일 추가

---
+++ problems/baekjoon/01_입출력과 사칙연산/10869_사칙연산/bohwa.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+let num1 = 7
+let num2 = 3
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(parseInt(num1 / num2));
+console.log(num1 % num2);
+
+// 실수를 정수로 만들어주기 위해 parseInt를 사용하였다
