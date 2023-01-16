1dcfbb6ae87dc93e73a2d0dc7e1987593a195199
jaesukpark77 <jaesukpark77@naver.com>
2023-01-17 00:19:39 +0900

    [solve : 2566] 박재석

--- problems/baekjoon/09_2차원 배열/2566_최댓값/jaeseok.py
+++ problems/baekjoon/09_2차원 배열/2566_최댓값/jaeseok.py
@@ -1,4 +1,5 @@
-board = []
+# 1. for문을 활용한 풀이
+""" board = []
 
 for _ in range(9):
   board.append(list(map(int, input().split())))
@@ -14,4 +15,17 @@ for r in range(9):
       row = r + 1
       col = c + 1
 print(MAX)
-print(row, col)
+print(row, col) """
+
+# enumerate 활용한 풀이
+arr = []
+MAX = -99999
+max_idx = ''
+for r in range(9):
+  for c, num in enumerate(list(map(int, input().split()))):
+    if num > MAX:
+      MAX = num
+      max_idx = f'{r+1} {c+1}'
+
+print(MAX)
+print(max_idx)
